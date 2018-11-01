401e3ea33de592da1a9a8103f49a228b287342ff
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2018-11-01 23:01:45 +0300

    Implement maze algorithm

--- lab4/main.c
+++ lab4/main.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_STACK_SIZE 100 /*maximum stack size*/
-typedef struct n{
-short int row;
-short int col;
-short int dir;
+#define M 11
+#define P 15
+#define ROW M + 2
+#define COL P + 2
+#define MOVE_COUNT 8
+
+int maze[ROW][COL] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1},
+    {1,1,0,0,0,1,1,0,1,1,1,0,0,1,1,1,1},
+    {1,0,1,1,0,0,0,0,1,1,1,1,0,0,1,1,1},
+    {1,1,1,0,1,1,1,1,0,1,1,0,1,1,0,0,1},
+    {1,1,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1},
+    {1,0,0,1,1,0,1,1,1,0,1,0,0,1,0,1,1},
+    {1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1},
+    {1,0,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1},
+    {1,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,1},
+    {1,0,0,1,1,1,1,1,0,0,0,1,1,1,1,0,1},
+    {1,0,1,0,0,1,1,1,1,1,0,1,1,1,1,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+};
+
+int mark[ROW][COL];
+typedef struct {
+    short int row;
+    short int col;
+    short int dir;
 } element;
+
+typedef struct {
+    short int vert;
+    short int horiz;
+} offsets;
+offsets moves[MOVE_COUNT] = {
+    {-1, 0}, // N
+    {-1, 1}, // NE
+    {0, 1},  // E
+    {1, 1},  // SE
+    {1, 0},  // S
+    {1, -1}  // SW
+    {0, -1}  // W
+    {-1, -1} // NW
+}; /*array of moves for each direction*/
+
+
+
 element *stack[MAX_STACK_SIZE];
 int top = -1;
-int isempty() {
+int isEmpty() {
 
    if(top == -1)
       return 1;
@@ -16,7 +57,7 @@ int isempty() {
       return 0;
 }
 
-int isfull() {
+int isFull() {
 
    if(top == MAX_STACK_SIZE)
       return 1;
@@ -30,7 +71,7 @@ int peek() {
 int pop() {
 
 
-   if(!isempty()) {
+   if(!isEmpty()) {
 
       top = top - 1;
 
@@ -39,14 +80,16 @@ int pop() {
    }
 }
 
-void push(int data) {
+void push(int row, int col, int dir) {
 
-   if(!isfull()) {
-      top = top + 1;
-     stack[top] = data;
-   } else {
-      printf("Could not insert data, Stack is full.\n");
-   }
+    if(!isFull()) {
+        top = top + 1;
+        stack[top].row = row;
+        stack[top].col = col;
+        stack[top].dir = dir;
+    } else {
+        printf("Could not insert data, Stack is full.\n");
+    }
 }
 void display ()
 {
@@ -66,10 +109,62 @@ void display ()
     }
     printf ("\n");
 }
+
+void deepCopyMazeToMark() {
+    int i,j;
+    for(i = 0; i < ROW; i++) {
+        for(j = 0; j < COL; j++) {
+            mark[i][j] = maze[i][j];
+        }
+    }
+}
+
+int isExit(int currRow, int currCol) {
+    return currRow == M && currCol == P;
+}
+
+int canMove(int currRow, int currCol) {
+    return mave[currRow][currCol] == 0 && mark[currRow][currCol] == 0;
+}
+
+int solveMaze() {
+    element current;
+    offsets move;
+    int dir, nextRow, nextCol;
+    //initialize a stack to the maze's entrance coordinates
+    //and direction to north;
+    while (!isEmpty()) {
+        //move to position at top of stack
+        current = pop();
+
+        for(dir = 0; dir < MOVE_COUNT; dir++) {
+            move = moves[dir];
+
+            nextRow = current.row + move.row;
+            nextCol = current.col + move.col;
+
+            if(isExit(nextRow, nextCol)) {
+                return 1;
+            } else if (canMove(nextRow, nextCol)) {
+                //legal move and haven't been there
+                mark[nextRow][nextCol] = 1;
+                push(nextRow, nextCol, dir);
+            }
+        } 
+    }
+    printf("No path found\n");
+    return 0;
+}
+
+
 int main()
 {
-push(1);
-push(2);
-display();
+    int result;
+
+    deepCopyMazeToMark();
+    result = solveMaze();
+    if (result) {
+        printf("Solved\n");
+    }
     return 0;
 }
